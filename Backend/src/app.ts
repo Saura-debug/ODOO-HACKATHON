8a2776d053a0b0a95ce31db32e451b17c3082ca8
sudhanshu <sp7017184@gmail.com>
2026-07-12 12:28:51 +0530

    Tested seeding

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,31 +3,15 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
-import { prisma } from "./config/prisma";
 
-
-
-console.log("everything is working fine");
-app.get("/health", async (_, res) => {
-  try {
-    await prisma.$queryRaw`SELECT 1`;
-
-    res.json({
-      success: true,
-      database: "Connected ✅",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      database: "Disconnected ❌",
-    });
-  }
-});
+app.use("/api", routes);
 
 export default app;
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { prisma } from "../config/prisma";
 
 const router = Router();
 
@@ -7,6 +8,21 @@ router.get("/", (_, res) => {
     success: true,
     message: "TransitOps Backend Running 🚀",
   });
+});
+router.get("/health", async (_, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+
+    res.json({
+      success: true,
+      database: "Connected",
+    });
+  } catch {
+    res.status(500).json({
+      success: false,
+      database: "Disconnected",
+    });
+  }
 });
 
 export default router;
